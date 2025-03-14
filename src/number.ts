2b125a28388f3b6e3b9d216ccbc921d7ae75bcc0
hirotask <hirotaskhashi@gmail.com>
2025-03-15 00:23:46 +0900

    feat: add number utility functions

---
+++ src/number.ts
@@ -0,0 +1,87 @@
+export function sum(nums: number[]): number {
+    return nums.reduce((a, b) => {
+        return a + b;
+    }, 0);
+}
+
+export function average(nums: number[]): number {
+    return sum(nums) / nums.length;
+}
+
+export function max(nums: number[]): number {
+    return nums.reduce((a, b) => {
+        return Math.max(a, b);
+    });
+}
+
+export function min(nums: number[]): number {
+    return nums.reduce((a, b) => {
+        return Math.min(a, b);
+    });
+}
+
+export function maxOf(
+    nums: number[],
+    predicate: (it: number) => number
+): number {
+    return max(nums.map(predicate));
+}
+
+export function minOf(
+    nums: number[],
+    predicate: (it: number) => number
+): number {
+    return min(nums.map(predicate));
+}
+
+export function takeIf(
+    num: number,
+    predicate: (it: number) => boolean
+): number | null {
+    if (predicate(num)) {
+        return num;
+    }
+
+    return null;
+}
+
+export function takeUnless(
+    num: number,
+    predicate: (it: number) => boolean
+): number | null {
+    if (predicate(num)) {
+        return null;
+    }
+
+    return num;
+}
+
+export function range(start: number, end: number): number[] {
+    const result: number[] = [];
+
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    }
+
+    return result;
+}
+
+export function until(start: number, end: number): number[] {
+    const result: number[] = [];
+
+    for (let i = start; i < end; i++) {
+        result.push(i);
+    }
+
+    return result;
+}
+
+export function downTo(start: number, end: number): number[] {
+    const result: number[] = [];
+
+    for (let i = start; i >= end; i--) {
+        result.push(i);
+    }
+
+    return result;
+}
